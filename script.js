3f7646bdac7eb3f5d265d17d0fb9a1d9d8ed840b
csr <camilaseemannramos@gmail.com>
2023-02-09 21:52:20 +0100

    Navbar mobile fixes

--- script.js
+++ script.js
@@ -1,49 +1,56 @@
-// Set current Year
+// Set current year
 const yearEl = document.querySelector(".year");
 const currentYear = new Date().getFullYear();
 yearEl.textContent = currentYear;
 
-//Make mobile navigation work
+///////////////////////////////////////////////////////////
+// Make mobile navigation work
+
 const btnNavEl = document.querySelector(".btn-mobile-nav");
 const headerEl = document.querySelector(".header");
+
 btnNavEl.addEventListener("click", function () {
   headerEl.classList.toggle("nav-open");
 });
 
-//Smooth scrolling animation for all browsers
+///////////////////////////////////////////////////////////
+// Smooth scrolling animation
+
 const allLinks = document.querySelectorAll("a:link");
+
 allLinks.forEach(function (link) {
   link.addEventListener("click", function (e) {
     e.preventDefault();
     const href = link.getAttribute("href");
 
-    //Scroll back to top
+    // Scroll back to top
     if (href === "#")
       window.scrollTo({
         top: 0,
         behavior: "smooth",
       });
 
-    //Scroll to other links
+    // Scroll to other links
     if (href !== "#" && href.startsWith("#")) {
       const sectionEl = document.querySelector(href);
-      sectionEl.scrollIntoView({
-        behavior: "smooth",
-      });
+      sectionEl.scrollIntoView({ behavior: "smooth" });
     }
 
-    //Close mobile navigation
+    // Close mobile naviagtion
     if (link.classList.contains("main-nav-link"))
       headerEl.classList.toggle("nav-open");
   });
 });
 
-//Sticky navigation
+///////////////////////////////////////////////////////////
+// Sticky navigation
+
 const sectionHeroEl = document.querySelector(".section-hero");
 
 const obs = new IntersectionObserver(
   function (entries) {
     const ent = entries[0];
+    console.log(ent);
 
     if (ent.isIntersecting === false) {
       document.body.classList.add("sticky");
@@ -54,7 +61,8 @@ const obs = new IntersectionObserver(
     }
   },
   {
-    root: null, //In the viewport
+    // In the viewport
+    root: null,
     threshold: 0,
     rootMargin: "-80px",
   }
